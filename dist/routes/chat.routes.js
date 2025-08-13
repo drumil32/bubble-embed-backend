8e48a3fe65c7834dacf7cbded7eeda4ca682107c
drumil32 <akheniad@gmail.com>
2025-08-13 12:14:17 +0530

    build deleted

--- dist/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chatRoutes = void 0;
-const express_1 = require("express");
-const chat_controller_1 = require("../controllers/chat.controller");
-const router = (0, express_1.Router)();
-exports.chatRoutes = router;
-router.post('/chat', chat_controller_1.chat);
--- dist/services/chatAgent.service.js
+++
@@ -1,9 +0,0 @@
-var AgentTypes;
-(function (AgentTypes) {
-    AgentTypes[AgentTypes["OPENAI"] = 0] = "OPENAI";
-    AgentTypes[AgentTypes["DEEPSEEK"] = 1] = "DEEPSEEK";
-})(AgentTypes || (AgentTypes = {}));
-export class ChatAgentService {
-    static async init() {
-    }
-}
